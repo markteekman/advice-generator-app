517a69c9679e19ab3ffb30f352d44884b957d056
Mark Teekman <mark.teekman@gmail.com>
2022-08-30 15:14:43 +0200

    Update Astro version and Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://markteekman.github.io/',
-  base: 'advice-generator-app',
+  site: 'https://markteekman.github.io',
+  base: '/advice-generator-app',
   vite: {
     ssr: {
       external: ['svgo']
